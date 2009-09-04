0f41df5399b8fdac7e8aa40170f101f6b14d5d32
Diego Feitosa <dnfeitosa@gmail.com>
2009-09-04 16:52:27 -0300

    interface for table's layout

---
+++ NET Selenium DSL/Table/Layout/ITableLayout.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Caelum.SeleniumDSL.Table.Layout
+{
+    public interface ITableLayout
+    {
+        string HeaderValue(int Column);
+
+        string HeaderLinkValue(int Column);
+
+        int GetContentCount();
+
+        string Value(int Row, int Column);
+
+        int GetColumnCount();
+
+        int GetRowCount();
+
+        bool Contains(ITable Table, string Column, string Content);
+    }
+}

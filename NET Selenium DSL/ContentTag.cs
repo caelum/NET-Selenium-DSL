a94e57248c71fa840c12dfb16d90e43159e67a80
Diego Feitosa <dnfeitosa@gmail.com>
2009-09-04 09:44:09 -0300

    implementation of content tags

---
+++ NET Selenium DSL/ContentTag.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Selenium;
+
+namespace Caelum.SeleniumDSL
+{
+    public class ContentTag : IContentTag
+    {
+
+        private ISelenium selenium;
+        private string id;
+
+        public ContentTag(ISelenium Selenium, string Id)
+        {
+            this.selenium = Selenium;
+            this.id = Id;
+        }
+
+        public bool Contains(string Content)
+        {
+            return selenium.GetText(id).Contains(Content);
+        }
+
+        public bool Exists()
+        {
+            return selenium.IsElementPresent(id);
+        }
+
+        public string InnerHTML()
+        {
+            return selenium.GetText(id);
+        }
+
+    }
+}

78168515b4fec08adf688b30142f8f44998e051d
Diego Feitosa <dnfeitosa@gmail.com>
2009-09-09 19:07:53 -0300

    handling not found resources

--- Integration Tests/FileReaderModule.cs
+++ Integration Tests/FileReaderModule.cs
@@ -3,6 +3,7 @@ using HttpServer.HttpModules;
 using HttpServer;
 using HttpServer.Sessions;
 using System.Collections.Generic;
+using System;
 namespace Integration.Caelum.SeleniumDSL
 {
     public class FileReaderModule : HttpModule
@@ -12,17 +13,30 @@ namespace Integration.Caelum.SeleniumDSL
         {
             ignoredPaths = new List<string>();
             ignoredPaths.Add("/favicon.ico");
+            ignoredPaths.Add("/");
         }
 
         public override bool Process(IHttpRequest request, IHttpResponse response, IHttpSession session)
         {
-            if (ignoredPaths.Contains(request.Uri.AbsolutePath))
+            if  (ignoredPaths.Contains(request.Uri.AbsolutePath))
                 return true;
 
-            string fileContents = File.ReadAllText("../../../Integration Tests/resources" + request.Uri.AbsolutePath);
             TextWriter writer = new StreamWriter(response.Body);
-            writer.Write(fileContents);
-            writer.Close();
+            try
+            {
+                string fileContents = File.ReadAllText("../../../Integration Tests/resources" + request.Uri.AbsolutePath);
+                writer.Write(fileContents);
+            }
+            catch (Exception ex)
+            {
+                response.ContentType = "text/plain; charset=UTF-8";
+                writer.WriteLine(ex.Message);
+                writer.WriteLine(ex.StackTrace);
+            }
+            finally
+            {
+                writer.Flush();
+            }
 
             return true;
         }

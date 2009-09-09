7d22a3e5045d73fe30a1dbe7468876e921558d29
Diego Feitosa <dnfeitosa@gmail.com>
2009-09-09 20:04:14 -0300

    test refactor and Field test implementation

---
+++ NET Selenium DSL Tests/FieldTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Selenium;
+using Caelum.SeleniumDSL;
+
+namespace Unit.Caelum.SeleniumDSL
+{
+    [TestClass]
+    public class FieldTest : UnitTest
+    {
+        [TestMethod]
+        public void TestBlur()
+        {
+            selenium.Setup(o => o.FireEvent("", "blur"));
+
+            new Field(selenium.Object, null, "").Blur();
+        }
+
+        [TestMethod]
+        public void TestChange()
+        {
+            selenium.Setup(o => o.FireEvent("", "change"));
+
+            new Field(selenium.Object, null, "").Change();
+        }
+
+        [TestMethod]
+        public void TestContains()
+        {
+            selenium.Setup(o => o.GetValue("someId"))
+                .Returns("Some text");
+            
+            Field field = new Field(selenium.Object, null, "someId");
+
+            Assert.IsTrue(field.Contains("text"));
+        }
+
+        [TestMethod]
+        public void TestContent()
+        {
+            selenium.Setup(o => o.GetValue("someId"))
+                .Returns("Some text");
+
+            Field field = new Field(selenium.Object, null, "someId");
+
+            Assert.AreEqual("Some text", field.Content());
+        }
+
+        [TestMethod]
+        public void TestType()
+        {
+            selenium.Setup(o => o.Type("someId", "Some text"));
+
+            Field field = new Field(selenium.Object, null, "someId");
+
+            field.Type("Some text");
+        }
+
+    }
+}
---
+++ NET Selenium DSL Tests/UnitTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Moq;
+using Selenium;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unit.Caelum.SeleniumDSL
+{
+    public class UnitTest
+    {
+        protected MockFactory factory;
+        protected Mock<ISelenium> selenium;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            factory = new MockFactory(MockBehavior.Default);
+            selenium = factory.Create<ISelenium>();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            selenium.VerifyAll();
+        }
+    }
+}

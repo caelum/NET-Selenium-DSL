325a7bcfe8fde73d512ade19b1a2dfbd81379ae6
Diego Feitosa <dnfeitosa@gmail.com>
2009-09-25 19:36:27 -0300

    paramenter names

---
+++ NET Selenium DSL/Table/Column.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Selenium;
+
+namespace Caelum.SeleniumDSL.Table
+{
+    internal class Column : IColumn
+    {
+        private ISelenium selenium;
+        private int columnNumber;
+
+        public Column(ISelenium selenium, int columnNumber)
+        {
+            this.selenium = selenium;
+            this.columnNumber = columnNumber;
+        }
+
+        public bool Contains(string Text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsPartial(string Value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Find(string Text)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}

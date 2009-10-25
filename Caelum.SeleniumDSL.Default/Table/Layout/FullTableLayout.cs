36ca1a28e34d6059c41b1d2658be6aaad623d092
Diego Feitosa <dnfeitosa@gmail.com>
2009-10-25 18:11:23 -0200

    code style

--- Caelum.SeleniumDSL.Default/Table/Layout/FullTableLayout.cs
+++ Caelum.SeleniumDSL.Default/Table/Layout/FullTableLayout.cs
@@ -6,45 +6,45 @@ namespace Caelum.SeleniumDSL.Default.Table.Layout
 {
     internal class FullTableLayout : ITableLayout
     {
-        private readonly ISelenium selenium;
-        private readonly string tableXPath;
-        private readonly string headerXPath;
+        private readonly ISelenium _selenium;
+        private readonly string _tableXPath;
+        private readonly string _headerXPath;
 
         public FullTableLayout(ISelenium selenium, string id)
         {
-            this.selenium = selenium;
-            this.tableXPath = string.Format("//table[@id='{0}']", id);
-            this.headerXPath = tableXPath + "/thead/tr[1]";
+            _selenium = selenium;
+            _tableXPath = string.Format("//table[@id='{0}']", id);
+            _headerXPath = _tableXPath + "/thead/tr[1]";
         }
 
         public string HeaderValue(int column)
         {
-            return this.selenium.GetText(this.headerXPath + string.Format("/th[{0}]", column));
+            return _selenium.GetText(_headerXPath + string.Format("/th[{0}]", column));
         }
 
         public string HeaderLinkValue(int column)
         {
-            return this.selenium.GetText(this.headerXPath + string.Format("/th[{0}]/a/text()", column));
+            return _selenium.GetText(_headerXPath + string.Format("/th[{0}]/a/text()", column));
         }
 
         public int GetContentCount()
         {
-            return (int) selenium.GetXpathCount(this.tableXPath + "/tbody/tr");
+            return (int) _selenium.GetXpathCount(_tableXPath + "/tbody/tr");
         }
 
         public string Value(int row, int column)
         {
-            return this.selenium.GetText(this.tableXPath + string.Format("/tbody/tr[{0}]/td[{1}]", row, column));
+            return _selenium.GetText(_tableXPath + string.Format("/tbody/tr[{0}]/td[{1}]", row, column));
         }
 
         public int GetColumnCount()
         {
-            return (int) selenium.GetXpathCount(this.tableXPath + "/thead/tr/th");
+            return (int) _selenium.GetXpathCount(_tableXPath + "/thead/tr/th");
         }
 
         public int GetRowCount()
         {
-            return (int) selenium.GetXpathCount(this.tableXPath + "/*/tr");
+            return (int) _selenium.GetXpathCount(_tableXPath + "/*/tr");
         }
 
         public bool Contains(ITable table, string column, string content)

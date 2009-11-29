9bed8762c73bedd9a53164a5df33a85c1ebc2fbd
Diego Feitosa <dnfeitosa@gmail.com>
2009-11-29 14:09:44 -0200

    test for form submission

---
+++ Caelum.SeleniumDSL.Default.Tests.Integration/FormTest.cs
@@ -0,0 +1,20 @@
+﻿using Caelum.SeleniumDSL.Default.Selector;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Caelum.SeleniumDSL.Default.Tests.Integration
+{
+    [TestClass]
+    public class FormTest : IntegrationTest
+    {
+        [TestMethod]
+        public void CanSubmitForm()
+        {
+            OpenForm();
+
+            IPage page = Browser.CurrentPage();
+            page.Form(Id.Is("testform")).Submit();
+
+            Assert.IsTrue(page.Div("ok").Exists());
+        }
+    }
+}
